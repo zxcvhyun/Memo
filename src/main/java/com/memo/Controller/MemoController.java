10d7ad84dc2a68ac7b0958838f150be12dcbd1fd
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-06-19 02:46:48 +0900

    20210619-테스트객체생성

--- src/main/java/com/memo/Controller/MemoController.java
+++ src/main/java/com/memo/Controller/MemoController.java
@@ -2,6 +2,8 @@
 package com.memo.Controller;
 
 import com.memo.dto.TestVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemoController.class);
+
     @RequestMapping("/hello")
     public String Hello() {
         return "Hello";
@@ -21,6 +26,8 @@ public class MemoController {
     @RequestMapping(value = "/testVO", method = RequestMethod.POST)
     public ModelAndView test1(HttpServletRequest httpServletRequest, Model model) {
         String username = httpServletRequest.getParameter("name");
+        System.out.println("username" + username);
+
         ModelAndView mav = new ModelAndView();
         mav.addObject("username", username);
         return mav;
