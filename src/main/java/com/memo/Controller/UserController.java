3362d59a0bcb7ff7c4383493b368ab0c9833c7d3
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-06-24 22:51:08 +0900

    20210624 - 회원가입 비밀번호 Hashing 추가

--- src/main/java/com/memo/Controller/UserController.java
+++ src/main/java/com/memo/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.memo.domain.entity.MemberEntity;
 import com.memo.model.InfoVO;
 //import com.memo.repository.MemberRepository;
 import com.memo.repository.MemberRepository;
+import com.memo.util.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,25 +41,7 @@ public class UserController {
 
 
     }
-    public static class Hashing {
-        public static final String SALT = "!@salt$%^&";
 
-        public static String hashingPassword(String input) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA-256");
-                byte[] hashData = md.digest(input.getBytes(StandardCharsets.UTF_8));
-                BigInteger number = new BigInteger(1, hashData);
-                StringBuilder hexString = new StringBuilder(number.toString(16));
-
-                while (hexString.length() < 32) {
-                    hexString.insert(0, '0');
-                }
-                return hexString.toString();
-            } catch (NoSuchAlgorithmException e) {
-                return input;
-            }
-        }
-    }
 }
 
 
